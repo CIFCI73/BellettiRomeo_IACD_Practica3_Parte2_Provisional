4d0d603a97930b240dcf3b5cb2e75673e0588bf1
Cristian Romeo <cristian11romeo@gmail.com>
2026-05-08 13:25:59 +0200

    definicion de acciones: grasp, climb, push y walk

--- Trabajo-Práctico-MÓDULO-3.pl
+++ Trabajo-Práctico-MÓDULO-3.pl
@@ -21,3 +21,27 @@ valid_pos(X, Y) :-
     X >= 0, X < N,
     Y >= 0, Y < N.
 
+% ACCIÓN 1: Agarrar el plátano (grasp)
+% Requisito: El mono debe estar sobre la caja, y ambos justo debajo del plátano[cite: 15].
+move(state(pos(X,Y), on-box, pos(X,Y), pos(X,Y), hasnot), grasp, 
+     state(pos(X,Y), on-box, pos(X,Y), pos(X,Y), has)).
+
+% ACCIÓN 2: Subir a la caja (climb)
+% Requisito: El mono y la caja están en la misma posición, y el mono está en el suelo[cite: 14].
+move(state(pos(X,Y), on-floor, pos(X,Y), PosB, Has), climb, 
+     state(pos(X,Y), on-box, pos(X,Y), PosB, Has)).
+
+% ACCIÓN 3: Empujar la caja (push)
+% Requisito: Mono en el suelo y en la misma posición que la caja. Ambos se mueven juntos[cite: 14].
+move(state(pos(X,Y), on-floor, pos(X,Y), PosB, Has), push(Dir), 
+     state(pos(NX,NY), on-floor, pos(NX,NY), PosB, Has)) :-
+    direction(Dir, X, Y, NX, NY),
+    valid_pos(NX, NY).
+
+% ACCIÓN 4: Caminar (walk)
+% Requisito: Mono en el suelo. Se mueve a una casilla adyacente vacía[cite: 13].
+move(state(pos(X,Y), on-floor, PosC, PosB, Has), walk(Dir), 
+     state(pos(NX,NY), on-floor, PosC, PosB, Has)) :-
+    direction(Dir, X, Y, NX, NY),
+    valid_pos(NX, NY).
+
