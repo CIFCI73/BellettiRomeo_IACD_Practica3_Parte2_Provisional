a75aa0c07324493cd1fcd1c9e3416ca613081d56
Cristian Romeo <cristian11romeo@gmail.com>
2026-05-08 12:53:46 +0200

    Definicion estado inicial y final

---
+++ Trabajo-Práctico-MÓDULO-3.pl
@@ -0,0 +1,7 @@
+% Define la dimensión N de la habitación (por ejemplo, 5x5)
+dimension(5).
+% ESTADO INICIAL (Caso 1)
+% state(pos(0,0), on-floor, pos(2,2), pos(1,1), hasnot)
+
+% ESTADO FINAL OBJETIVO
+% state(pos(1,1), on-box, pos(1,1), pos(1,1), has)
