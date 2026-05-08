732e3d8ed8e3db071db8aeecf91c1c26d5211f4e
Cristian Romeo <cristian11romeo@gmail.com>
2026-05-08 13:03:34 +0200

    logica de coordinadas y limites para no salir del espacio

--- Trabajo-Práctico-MÓDULO-3.pl
+++ Trabajo-Práctico-MÓDULO-3.pl
@@ -5,3 +5,19 @@ dimension(5).
 
 % ESTADO FINAL OBJETIVO
 % state(pos(1,1), on-box, pos(1,1), pos(1,1), has)
+
+% Hecho inicial: Dimensión de la habitación
+dimension(5).
+
+% Lógica de coordenadas según la dirección elegida
+direction(north, X, Y, X, Y_new) :- Y_new is Y + 1.
+direction(south, X, Y, X, Y_new) :- Y_new is Y - 1.
+direction(east,  X, Y, X_new, Y) :- X_new is X + 1.
+direction(west,  X, Y, X_new, Y) :- X_new is X - 1.
+
+% Comprobación para no salir de los límites de la habitación N x N
+valid_pos(X, Y) :-
+    dimension(N),
+    X >= 0, X < N,
+    Y >= 0, Y < N.
+
